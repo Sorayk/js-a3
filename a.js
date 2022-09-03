470afa4424d536138c12153433f3635f702ced48
Sorayk <86656971+Sorayk@users.noreply.github.com>
2022-09-03 13:50:03 +0200

    jsassignment2

---
+++ a.js
@@ -0,0 +1,10 @@
+function largerNumber(num1, num2) {
+    if (num1 > num2) {
+        console.log("num1 is bigger than num2")
+    }
+    else if (num1 < num2) {
+        console.log("num2 is bigger than num1")
+    }
+}
+
+largerNumber(2, 3);
---
+++ b.js
@@ -0,0 +1,9 @@
+function myNumbers(num1) {
+    if (num1 % 2 == 0) {
+        console.log("Even")
+    } else if (num1 % 2 != 0) {
+        console.log("Odd")
+    }
+};
+
+myNumbers(989824986);
